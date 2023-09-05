4864fe1063eba1a9e2de5e9b427a1f38e264e58b
liangxiaoming <liangxm@tianyisc.com>
2023-09-05 10:56:20 +0800

    add

---
+++ get_https.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+#define SKIP_PEER_VERIFICATION 
+#define SKIP_HOSTNAME_VERIFICATION
+
+// 回调函数，用于处理Curl的响应数据
+size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+    size_t total_size = size * nmemb;
+    printf("Received data: %d\n", (int)total_size);
+    return total_size;
+}
+
+ 
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+ 
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://127.0.0.1/file/5M.bin");
+ 
+#ifdef SKIP_PEER_VERIFICATION
+    /*
+     * If you want to connect to a site who is not using a certificate that is
+     * signed by one of the certs in the CA bundle you have, you can skip the
+     * verification of the server's certificate. This makes the connection
+     * A LOT LESS SECURE.
+     *
+     * If you have a CA cert for the server stored someplace else than in the
+     * default bundle, then the CURLOPT_CAPATH option might come handy for
+     * you.
+     */
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+#endif
+ 
+#ifdef SKIP_HOSTNAME_VERIFICATION
+    /*
+     * If the site you are connecting to uses a different host name that what
+     * they have mentioned in their server certificate's commonName (or
+     * subjectAltName) fields, libcurl will refuse to connect. You can skip
+     * this check, but this will make the connection less secure.
+     */
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+#endif
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+ 
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+ 
+  curl_global_cleanup();
+ 
+  return 0;
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# 线程安全  
+libcurl自称是线程安全，但看网上还是说有踩坑的。  
+**要点如下：**  
+* 设置CURLOPT_NOSIGNAL为1, `easy_setopt(curl, CURLOPT_NOSIGNAL, (long)1);`  
+* 程序一开始就调用curl_global_init(CURL_GLOBAL_DEFAULT);  
